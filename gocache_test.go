2db7a30864c5e5af5cda6374ad3a01a5c2b21dab
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-07-25 20:37:49 +0900

    added: test case for Get and Set

--- gocache_test.go
+++ gocache_test.go
@@ -53,3 +53,35 @@ func TestIsValid(t *testing.T) {
 		}
 	}
 }
+
+func TestGet(t *testing.T) {
+	tests := []struct {
+		key      interface{}
+		val      interface{}
+		expected interface{}
+	}{
+		{
+			key:      "key-1",
+			val:      "key-1_value",
+			expected: "key-1_value",
+		},
+	}
+
+	g := New()
+
+	for i, test := range tests {
+		ok := g.Set(test.key, test.val)
+		if !ok {
+			t.Errorf("tests[%d] - Set ok is wrong. expected: %v, got: %v", i, true, ok)
+		}
+
+		got, ok := g.Get(test.key)
+		if !ok {
+			t.Errorf("tests[%d] - Get ok is wrong. expected: %v, got: %v", i, true, ok)
+		}
+
+		if got != test.expected {
+			t.Errorf("tests[%d] - Get is wrong. expected: %v, got: %v", i, test.expected, got)
+		}
+	}
+}
