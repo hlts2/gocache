0f89753e278cf3d4f154720e744f2cceb3dcc8df
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-07-27 03:07:09 +0900

    added: other package for bench mark test

--- gocache_bench_test.go
+++ gocache_bench_test.go
@@ -4,17 +4,23 @@ import (
 	"testing"
 	"time"
 
+	"github.com/bluele/gcache"
 	"github.com/bouk/monkey"
+	"github.com/kpango/gache"
 	"github.com/patrickmn/go-cache"
 )
 
 var data = map[string]string{
 	"key_1": "key_1_value",
+	"key_2": "key_1_value",
+	"key_3": "key_1_value",
+	"key_4": "key_1_value",
 }
 
 func BenchmarkGocache(b *testing.B) {
 	monkey.Unpatch(time.Now)
 	g := New()
+	g.StopDeleteExpired()
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
@@ -35,6 +41,34 @@ func BenchmarkGo_cache(b *testing.B) {
 		for key, val := range data {
 			c.Set(key, val, time.Second*50)
 			c.Get(key)
+		}
+	}
+}
+
+func BenchmarkGoacheARC(b *testing.B) {
+	monkey.Unpatch(time.Now)
+	gc := gcache.New(10).Build()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for key, val := range data {
+			gc.Set(key, val)
+			gc.Get(key)
+		}
+	}
+}
+
+func BenchmarkGache(b *testing.B) {
+	monkey.Unpatch(time.Now)
+	gc := gache.New()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for key, val := range data {
+			gc.Set(key, val)
+			gc.Get(key)
 		}
 	}
 }
